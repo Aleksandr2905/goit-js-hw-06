84ae539a50a125273e392d369b8199bd1ff59be6
Aleksandr2905 <natalukha@ukr.net>
2023-08-26 23:42:59 +0300

    task 02

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector('#ingredients');
+const newArr = [];
+ingredients.forEach((el) => {
+  const item = document.createElement('li');  
+  item.classList.add('item');
+  item.textContent = el;
+  newArr.push(item);
+  });
+
+list.append(...newArr);
+
+
+
