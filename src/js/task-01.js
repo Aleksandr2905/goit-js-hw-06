56abfd6ddb1094407deb3b18c6335c9942176fce
Aleksandr2905 <natalukha@ukr.net>
2023-08-26 19:38:21 +0300

    src

--- src/js/task-01.js
+++
@@ -1,9 +0,0 @@
-function task1() {
-    const liItem = document.querySelectorAll('li.item');
-    console.log(`Number of categories: ${liItem.length}`);
-
-    liItem.forEach(el => {              
-        console.log(`Category: ${el.firstElementChild.textContent}\nElements: ${el.lastElementChild.children.length}`);
-    });
-}
-task1();
