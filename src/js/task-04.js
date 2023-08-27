869525b82077b5a0d5059f100b1b8d579e3eafcc
Aleksandr2905 <natalukha@ukr.net>
2023-08-27 19:56:03 +0300

    task 04

---
+++ src/js/task-04.js
@@ -0,0 +1,17 @@
+const counter = document.querySelector('#value');
+const oneMinus = document.querySelector('button[data-action="decrement"]');
+const onePlus = document.querySelector('button[data-action="increment"]');
+
+
+let counterValue = 0;
+const handleClickMinus = () => {
+    counterValue -= 1;
+    counter.textContent = counterValue;
+};
+oneMinus.addEventListener('click', handleClickMinus);
+
+const handleClickPlus = () => {
+    counterValue += 1;
+    counter.textContent = counterValue;
+};
+onePlus.addEventListener('click', handleClickPlus);
