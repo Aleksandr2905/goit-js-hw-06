7137d0cb4a51fec6e98f9b75610336ab6292f6ca
Aleksandr2905 <138802205+Aleksandr2905@users.noreply.github.com>
2023-08-27 08:45:51 +0000

    Deploying to gh-pages from @ Aleksandr2905/goit-js-hw-06@1ad7e7bbcbbe54c636b54aad1c21f40ebd344ead 🚀

--- task-02.373c42d6.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("#ingredients"),t=[];["Potatoes","Mushrooms","Garlic","Tomatos","Herbs","Condiments"].forEach((e=>{const o=document.createElement("li");o.classList.add("item"),o.textContent=e,t.push(o)})),e.append(...t);
-//# sourceMappingURL=task-02.373c42d6.js.map
