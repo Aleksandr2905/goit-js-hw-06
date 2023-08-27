8fe64e7b41b95f2a538cc176ee5f5735f64afa43
Aleksandr2905 <natalukha@ukr.net>
2023-08-27 22:10:40 +0300

    додавання функціїї task4

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -2,16 +2,18 @@ const counter = document.querySelector('#value');
 const oneMinus = document.querySelector('button[data-action="decrement"]');
 const onePlus = document.querySelector('button[data-action="increment"]');
 
+function task4 () {
+    let counterValue = 0;
+    const handleClickMinus = () => {
+        counterValue -= 1;
+        counter.textContent = counterValue;
+    };
+    oneMinus.addEventListener('click', handleClickMinus);
 
-let counterValue = 0;
-const handleClickMinus = () => {
-    counterValue -= 1;
-    counter.textContent = counterValue;
+    const handleClickPlus = () => {
+        counterValue += 1;
+        counter.textContent = counterValue;
+    };
+        onePlus.addEventListener('click', handleClickPlus);
 };
-oneMinus.addEventListener('click', handleClickMinus);
-
-const handleClickPlus = () => {
-    counterValue += 1;
-    counter.textContent = counterValue;
-};
-onePlus.addEventListener('click', handleClickPlus);
+task4();
