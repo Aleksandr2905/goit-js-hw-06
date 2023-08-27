1ad7e7bbcbbe54c636b54aad1c21f40ebd344ead
Aleksandr2905 <natalukha@ukr.net>
2023-08-27 11:45:30 +0300

    додавання функціїї до task2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,22 +1,26 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+function task2() {
+    const ingredients = [
+    'Potatoes',
+    'Mushrooms',
+    'Garlic',
+    'Tomatos',
+    'Herbs',
+    'Condiments',
+  ];
 
-const list = document.querySelector('#ingredients');
-const newArr = [];
-ingredients.forEach((el) => {
-  const item = document.createElement('li');  
-  item.classList.add('item');
-  item.textContent = el;
-  newArr.push(item);
-  });
+  const list = document.querySelector('#ingredients');
+  const newArr = [];
+  ingredients.forEach((el) => {
+    const item = document.createElement('li');  
+    item.classList.add('item');
+    item.textContent = el;
+    newArr.push(item);
+    });
 
-list.append(...newArr);
+  list.append(...newArr);
+}
+task2();
+
 
 
 
