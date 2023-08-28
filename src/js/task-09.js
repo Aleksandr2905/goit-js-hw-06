07d584a0aee5b4cc49c70382c10e216905cd3684
Aleksandr2905 <natalukha@ukr.net>
2023-08-29 00:07:29 +0300

    task 09

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+const body = document.querySelector('body');
+const changeBtn = document.querySelector('.change-color');
+const spanColor = document.querySelector('.color');
+
+changeBtn.addEventListener('click', event => {
+  let color = getRandomHexColor();
+  body.style.backgroundColor = color;
+  spanColor.textContent = color;
+  
+});
