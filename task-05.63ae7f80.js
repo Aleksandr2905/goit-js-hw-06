c885a0d745b6e07297dd53c6fbacb34c9136ca82
Aleksandr2905 <138802205+Aleksandr2905@users.noreply.github.com>
2023-08-28 14:56:52 +0000

    Deploying to gh-pages from @ Aleksandr2905/goit-js-hw-06@c0da2e3166b387ea1479e703341ad0c84c6b9720 🚀

---
+++ task-05.63ae7f80.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("#name-input"),n=document.querySelector("#name-output");e.addEventListener("input",(t=>{""!==e.value?n.textContent=`${e.value}`:n.innerHTML="Anonymous"}));
+//# sourceMappingURL=task-05.63ae7f80.js.map
---
+++ task-05.76fd9c1b.js
@@ -0,0 +1,2 @@
+!function(){var n=document.querySelector("#name-input"),e=document.querySelector("#name-output");n.addEventListener("input",(function(t){""!==n.value?e.textContent="".concat(n.value):e.innerHTML="Anonymous"}))}();
+//# sourceMappingURL=task-05.76fd9c1b.js.map
