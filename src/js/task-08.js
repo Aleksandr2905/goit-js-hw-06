dfa598a7b88f7723600d84c92c46073f640648d0
Aleksandr2905 <natalukha@ukr.net>
2023-08-28 23:31:09 +0300

    task 08

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,15 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    const login = form.elements.email.value;
+    const password = form.elements.password.value;
+    if (login === '' || password === '') {
+        return alert('Всі поля повинні бути заповнені!');        
+    }
+    const data = {login, password};
+    console.log(data);
+    form.reset();
+}
