0b4be297800cd6f3c4d5ce7b7e7e312b6c6b73d5
Aleksandr2905 <138802205+Aleksandr2905@users.noreply.github.com>
2023-08-28 19:01:14 +0000

    Deploying to gh-pages from @ Aleksandr2905/goit-js-hw-06@7f2ccc25bf8723e3a7452636ac50d7554ce19b6b 🚀

---
+++ task-07.1fd03aed.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("#font-size-control"),t=document.querySelector("#text");e.addEventListener("change",(n=>{t.style.fontSize=`${e.value}px`}));
+//# sourceMappingURL=task-07.1fd03aed.js.map
---
+++ task-07.8512ba69.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("#font-size-control"),t=document.querySelector("#text");e.addEventListener("change",(function(n){t.style.fontSize="".concat(e.value,"px")}))}();
+//# sourceMappingURL=task-07.8512ba69.js.map
